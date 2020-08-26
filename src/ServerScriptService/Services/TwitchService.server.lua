23e7c71e5620834beebd87dc611d2fb9338335e8
CheekySquid <shuttle99@live.com>
2020-08-26 11:18:33 -0400

    Update TwitchService.server.lua

--- src/ServerScriptService/Services/TwitchService.server.lua
+++ src/ServerScriptService/Services/TwitchService.server.lua
@@ -3,7 +3,8 @@ local provuser = "cheekyvisuals"
 local URL = "https://api.twitch.tv/helix/streams?user_login=" .. tostring(provuser)
 local response
 local client_id_header = {
-    ["Client-ID"] = "gmr2upwb02k5i8qulff7p4c4fekomu"
+    ["Client-ID"] = "gmr2upwb02k5i8qulff7p4c4fekomu",
+    ["Client-SECRET"] = "su4e5u1poepne2wex5ejs2xbwryxo6"
 }
 local livestreaming
 local name
@@ -12,9 +13,14 @@ local title
 local game_id
 local catName
 function GetNewValues()
+    local resp
+    resp = HttpService:PostAsync("https://id.twitch.tv/oauth2/token?client_id=" .. tostring(client_id_header["Client-ID"]) .. "&client_secret="  .. tostring(client_id_header["Client-SECRET"]) .. "&grant_type=client_credentials", HttpService:JSONEncode({})) -- geting the oauth token documentation is bloody shite
+    local access_token = (HttpService:JSONDecode(resp)["access_token"])
+    
+    client_id_header["authorization"] = "Bearer " .. tostring(access_token) -- twitch are reatrded you have to put bearer b4 it
     response = HttpService:GetAsync(URL, true, client_id_header)
     response = game:GetService("HttpService"):JSONDecode(response)
-    for _,v in pairs(response["data"]) do
+    for i,v in pairs(response["data"]) do
         for i2, v2 in pairs(v) do
             if i2 == "type" then
                 livestreaming = v2
@@ -38,10 +44,12 @@ responseCat = game:GetService("HttpService"):JSONDecode(responseCat)
 for i,v in pairs(responseCat["data"]) do
     for i2, v2 in pairs(v) do
         if i2 == "name" then
+        
             catName = v2
         end
     end
 end
+print(catName)
 
 while wait(60) do
     GetNewValues()
