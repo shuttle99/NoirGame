086dcdf7c031fba019e8b1d2157fb14d2aff60c3
CheekySquid <shuttle99@live.com>
2020-08-26 10:22:07 -0400

    Finished TwitchService

--- src/ServerScriptService/Services/TwitchService.server.lua
+++ src/ServerScriptService/Services/TwitchService.server.lua
@@ -3,7 +3,7 @@ local provuser = "cheekyvisuals"
 local URL = "https://api.twitch.tv/helix/streams?user_login=" .. tostring(provuser)
 local response
 local client_id_header = {
-    ["Client-ID"] = "secret"
+    ["Client-ID"] = "gmr2upwb02k5i8qulff7p4c4fekomu"
 }
 local livestreaming
 local name
@@ -14,7 +14,7 @@ local catName
 function GetNewValues()
     response = HttpService:GetAsync(URL, true, client_id_header)
     response = game:GetService("HttpService"):JSONDecode(response)
-    for i,v in pairs(response["data"]) do
+    for _,v in pairs(response["data"]) do
         for i2, v2 in pairs(v) do
             if i2 == "type" then
                 livestreaming = v2
@@ -31,13 +31,6 @@ function GetNewValues()
     end
 end
 
-GetNewValues()
-print(livestreaming)
-print(name)
-print(views)
-print(title)
-print(game_id)
-print(catName)
 local URLCat = "https://api.twitch.tv/helix/games?id=" .. tostring(game_id) -- gets cat name from game id
 local responseCat
 responseCat = HttpService:GetAsync(URLCat, true, client_id_header)
@@ -49,4 +42,14 @@ for i,v in pairs(responseCat["data"]) do
         end
     end
 end
-print(catName)
+
+while wait(60) do
+    GetNewValues()
+    if livestreaming then
+        print("CheekyVisuals is currently live on Twitch working on Noir! Check him out at twitch.tv/CheekyVisuals for exclusive codes!")
+        wait(240)
+        if livestreaming then
+            print("CheekyVisuals is currently live on Twitch working on Noir! Check him out at twitch.tv/CheekyVisuals for exclusive codes!")
+        end
+    end
+end
