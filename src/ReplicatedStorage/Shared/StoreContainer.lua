10bf5a031362bac5d209bb66dc028c426fbd74dd
CheekySquid <shuttle99@live.com>
2020-09-02 12:22:16 -0400

    Added init method

--- src/ReplicatedStorage/Shared/StoreContainer.lua
+++ src/ReplicatedStorage/Shared/StoreContainer.lua
@@ -10,26 +10,28 @@ storeContainer.Knives = {}
 storeContainer.Sprays = {}
 storeContainer.Guns = {}
 
-for _, folder in pairs(assets.Items:GetChildren()) do
-	local reference
-	if folder.Name == "Murderer" then
-		reference = "Knives"
-	elseif folder.Name == "Vandal" then
-		reference = "Sprays"
-	elseif folder.Name == "Vigilante" then
-		reference = "Guns"
-	end
-	for _, item in pairs(folder:GetChildren()) do
-		if itemModels:FindFirstChild(item.Name) and item.Name ~= "Spray" then
-			local itemModel = itemModels:FindFirstChild(item.Name)
-			print(itemModel.Name)
-			storeContainer[reference][itemModel.Name] = {
-				["Name"] = itemModel:WaitForChild("Name").Value,
-				["Description"] = itemModel:WaitForChild("Description").Value,
-				["Price"] = itemModel:WaitForChild("Price").Value
-			}
-			if itemModel:FindFirstChild("Gamepass") then
-				storeContainer[reference][item.Name]["Gamepass"] = itemModel.Gamepass.Value
+function storeContainer:Init()
+	for _, folder in pairs(assets.Items:GetChildren()) do
+		local reference
+		if folder.Name == "Murderer" then
+			reference = "Knives"
+		elseif folder.Name == "Vandal" then
+			reference = "Sprays"
+		elseif folder.Name == "Vigilante" then
+			reference = "Guns"
+		end
+		for _, item in pairs(folder:GetChildren()) do
+			if itemModels:FindFirstChild(item.Name) and item.Name ~= "Spray" then
+				local itemModel = itemModels:FindFirstChild(item.Name)
+				print(itemModel.Name)
+				storeContainer[reference][itemModel.Name] = {
+					["Name"] = itemModel:WaitForChild("Name").Value,
+					["Description"] = itemModel:WaitForChild("Description").Value,
+					["Price"] = itemModel:WaitForChild("Price").Value
+				}
+				if itemModel:FindFirstChild("Gamepass") then
+					storeContainer[reference][item.Name]["Gamepass"] = itemModel.Gamepass.Value
+				end
 			end
 		end
 	end
