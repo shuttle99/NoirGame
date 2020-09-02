87f3c2dfb1939ed0350e283df446b119d474ec7f
CheekySquid <shuttle99@live.com>
2020-09-02 01:28:35 -0400

    Update StoreContainer.lua

--- src/ReplicatedStorage/Shared/StoreContainer.lua
+++ src/ReplicatedStorage/Shared/StoreContainer.lua
@@ -1,282 +1,43 @@
-local module = {}
+local storeContainer = {}
 
-module.Knives = {
-	["Knife"] = {
-		Name = "Butterfly Knife",
-		Description = "Just your everyday, average, innocent-slaughtering knife.",
-		Price = 0,
-	},
-	["Katana"] = {
-		Name = "Katana",
-		Description = "A legendary katana, only wielded by the bravest warriors.",
-		Price = 2000,
-	},
-	["GalacticSaber"] = {
-		Name = "Galactic Saber",
-		Description = "An elegant weapon reminiscent of a more civilized age.",
-		Price = 5000,
-	},
-	["LinkedSword"] = {
-		Name = "Linked Sword",
-		Description = "A true classic.",
-		Price = 4000,
-	},
-	["ERRORKnife"] = {
-		Name = "ERROR",
-		Description = "Just get CS:S, man!",
-		Price = 3000,
-	},
-	["FuturisticKunai"] = {
-		Name = "Futuristic Kunai",
-		Description = "Great for throwing and hand-to-hand combat.",
-		Price = 3000,
-		Gamepass = 11292444
-	},
-	["Axe"] = {
-		Name = "Axe",
-		Description = "A classic slasher weapon.",
-		Price = 500,
-	},
-	["FryingPan"] = {
-		Name = "Frying Pan",
-		Description = "I can almost hear the sizzle...",
-		Price = 600,
-	},
-	["Machete"] = {
-		Name = "Machete",
-		Description = "Ki ki ki, ma ma ma...",
-		Price = 500,
-	},
-	["Messer"] = {
-		Name = "Messer",
-		Description = "Just a really long knife if you think about it.",
-		Price = 850,
-	},
-	["Rapier"] = {
-		Name = "Rapier",
-		Description = "I know something you don’t know. I am not left-handed.",
-		Price = 1000,
-	},
-	["Cleaver"] = {
-		Name = "Cleaver",
-		Description = "Premium innocent meat comin' right up!",
-		Price = 250,
-	},
-	["BlockyBlade"] = {
-		Name = "Blocky Blade",
-		Description = "This looks oddly familiar...",
-		Price = 1500,
-	},
-	["ArrowHead"] = {
-		Name = "Arrow Head",
-		Description = "Used to retire adventurers.",
-		Price = 250,
-	},
-	["Bone"] = {
-		Name = "Bone",
-		Description = "I'd tell you my bone joke, but it's not that humerus.",
-		Price = 1000,
-	},
-	["Pencil"] = {
-		Name = "Pencil",
-		Description = "Trusty ol' no.2!",
-		Price = 1000,
-	},
-	["Fish"] = {
-		Name = "Fish",
-		Description = "This little guy's got some sharp fins! Part of the Fishy Business pack.",
-		Price = 1000,
-	},
-	["BetaSword"] = {
-		Name = "Beta Blade",
-		Description = "A weapon for the true OGs.",
-		Price = 1000,
-	},
-	["Bananknife"] = {
-		Name = "Bananknife",
-		Description = "I uh... really don't know what to say about this one.",
-		Price = 1000,
-	},
-	["2x4"] = {
-		Name = "2x4",
-		Description = "A nailed plank? Sounds good to me!",
-		Price = 750,
-	},
-	["BaseballBat"] = {
-		Name = "Baseball Bat",
-		Description = "BONK!",
-		Price = 750,
-	},
-	["Karambit"] = {
-		Name = "Karambit",
-		Description = "These are pretty fun to spin around.",
-		Price = 750,
-	},
-	["HiddenBlade"] = {
-		Name = "Hidden Blade",
-		Description = "An assassin's best friend.",
-		Price = 2500,
-	}
-}
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerStorage = game:GetService("ServerStorage")
 
-module.Guns = {
-	["Revolver"] = {
-		Name = "Revolver",
-		Description = "Just your everyday, average, murderer-slaying Revolver.",
-		Price = 0,
-	},
-	["HuntingRifle"] = {
-		Name = "Hunting Rifle",
-		Description = "It's murderer-huntin' season!",
-		Price = 750,
-	},
-	["BananaGun"] = {
-		Name = "Banana Gun",
-		Description = "pew pew pew pew pew pew pew... banana guuun...",
-		Price = 500,
-	},
-}
+local itemModels = ReplicatedStorage:WaitForChild("ItemModels")
+local assets = ServerStorage:WaitForChild("Assets")
 
-module.Sprays = {
-	["StandardSpray"] = {
-		Name = "Red Spray Paint",
-		Description = "Just your everyday, average, murderer-revealing spray paint.",
-		Price = 0, 
-	},
-	["OrangeSpray"] = {
-		Name = "Orange Spray Paint",
-		Description = "Insert orange-inal pun here.",
-		Price = 250, 
-	},
-	["YellowSpray"] = {
-		Name = "Yellow Spray Paint",
-		Description = "A bright color that's great for revealing.",
-		Price = 100, 
-	},
-	["GreenSpray"] = {
-		Name = "Green Spray Paint",
-		Description = "You either love plants or hate murderers.",
-		Price = 250, 
-	},
-	["TealSpray"] = {
-		Name = "Teal Spray Paint",
-		Description = "Named after a bird, believe it or not!",
-		Price = 250, 
-	},
-	["BlueSpray"] = {
-		Name = "Blue Spray Paint",
-		Description = "Reminds me of the ocean!",
-		Price = 100, 
-	},
-	["PurpleSpray"] = {
-		Name = "Purple Spray Paint",
-		Description = "Violet too, I guess...",
-		Price = 250, 
-	},
-	["WhiteSpray"] = {
-		Name = "White Spray Paint",
-		Description = "The absence of color, or all colors combined?",
-		Price = 100, 
-	},
-	["BlackSpray"] = {
-		Name = "Black Spray Paint",
-		Description = "Did you know Noir means black in french?",
-		Price = 100, 
-	},
-	["RainbowSpray"] = {
-		Name = "Rainbow Spray Paint",
-		Description = "Colorful!",
-		Price = 1250, 
-	},
-	["MonochromeSpray"] = {
-		Name = "Monochromatic Spray Paint",
-		Description = "Kinda spooky.",
-		Price = 800, 
-	},
-	["FireSpray"] = {
-		Name = "Fire Spray Paint",
-		Description = "Is it hot in here?",
-		Price = 2000, 
-	},
-	["IceSpray"] = {
-		Name = "Ice Spray Paint",
-		Description = "Brrrr...",
-		Price = 1500, 
-	},
-	["TicketSpray"] = {
-		Name = "Ticket Spray Paint",
-		Description = "They're back! Or well, in a can.",
-		Price = 1000, 
-	},
-	["GoldSpray"] = {
-		Name = "Gold Spray Paint",
-		Description = "Turn the murderer into solid gold! Part of the Golden Pack.",
-		Price = 0,
-		Gamepass = 11292412
-	}
-}
+storeContainer.Knives = {}
+storeContainer.Sprays = {}
+storeContainer.Guns = {}
 
-function module:LoopThroughItems()
-	for i, item in pairs(module.Guns) do
-		local name = Instance.new("StringValue")
-		local desc = Instance.new("StringValue")
-		local price = Instance.new("IntValue")
-		name.Value = item.Name
-		desc.Value = item.Description
-		price.Value = item.Price
-		name.Name = "Name"
-		desc.Name = "Description"
-		price.Name = "Price"
-		name.Parent = game.ReplicatedStorage.ItemModels[i]
-		desc.Parent = game.ReplicatedStorage.ItemModels[i]
-		price.Parent = game.ReplicatedStorage.ItemModels[i]
-		if item.Gamepass then
-			local gamepass = Instance.new("IntValue")
-			gamepass.Value = item.Gamepass
-			gamepass.Name = "Gamepass"
-			gamepass.Parent = game.ReplicatedStorage.ItemModels[i]
-		end
+for _, folder in pairs(assets.Items:GetChildren()) do
+	local reference
+	if folder.Name == "Murderer" then
+		reference = "Knives"
+	elseif folder.Name == "Vandal" then
+		reference = "Sprays"
+	elseif folder.Name == "Vigilante" then
+		reference = "Guns"
 	end
-	for i, item in pairs(module.Sprays) do
-		local name = Instance.new("StringValue")
-		local desc = Instance.new("StringValue")
-		local price = Instance.new("IntValue")
-		name.Value = item.Name
-		desc.Value = item.Description
-		price.Value = item.Price
-		name.Name = "Name"
-		desc.Name = "Description"
-		price.Name = "Price"
-		name.Parent = game.ReplicatedStorage.ItemModels[i]
-		desc.Parent = game.ReplicatedStorage.ItemModels[i]
-		price.Parent = game.ReplicatedStorage.ItemModels[i]
-		if item.Gamepass then
-			local gamepass = Instance.new("IntValue")
-			gamepass.Value = item.Gamepass
-			gamepass.Name = "Gamepass"
-			gamepass.Parent = game.ReplicatedStorage.ItemModels[i]
-		end
-	end
-	for i, item in pairs(module.Knives) do
-		local name = Instance.new("StringValue")
-		local desc = Instance.new("StringValue")
-		local price = Instance.new("IntValue")
-		name.Value = item.Name
-		desc.Value = item.Description
-		price.Value = item.Price
-		name.Name = "Name"
-		desc.Name = "Description"
-		price.Name = "Price"
-		name.Parent = game.ReplicatedStorage.ItemModels[i]
-		desc.Parent = game.ReplicatedStorage.ItemModels[i]
-		price.Parent = game.ReplicatedStorage.ItemModels[i]
-		if item.Gamepass then
-			local gamepass = Instance.new("IntValue")
-			gamepass.Value = item.Gamepass
-			gamepass.Name = "Gamepass"
-			gamepass.Parent = game.ReplicatedStorage.ItemModels[i]
+	for _, item in pairs(folder:GetChildren()) do
+		if itemModels:FindFirstChild(item.Name) and item.Name ~= "Spray" then
+			local itemModel = itemModels:FindFirstChild(item.Name)
+			print(itemModel.Name)
+			storeContainer[reference][itemModel.Name] = {
+				["Name"] = itemModel:WaitForChild("Name").Value,
+				["Description"] = itemModel:WaitForChild("Description").Value,
+				["Price"] = itemModel:WaitForChild("Price").Value
+			}
+			if itemModel:FindFirstChild("Gamepass") then
+				storeContainer[reference][item.Name]["Gamepass"] = itemModel.Gamepass.Value
+			end
 		end
 	end
 end
 
-return module
+--// To do 
+--[[
+	write an init method for this to be called only once in a service, then reference it otherwise to prevent serverstorage loading issue7
+]]
+
+return storeContainer
